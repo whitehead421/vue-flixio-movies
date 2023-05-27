7210a080df4b86a0cf489cd90128f2e6f9033751
whitehead421 <huseyinakbas@posta.mu.edu.tr>
2023-05-27 22:57:29 +0300

    featured movie re-designed

--- src/components/FeaturedMovie.vue
+++ src/components/FeaturedMovie.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="container text-white">
+  <div class="relative container text-white">
     <div class="group relative cursor-pointer overflow-hidden rounded-xl">
+      <!-- Image Div -->
       <div class="h-96 w-full">
         <img
           :src="`https://image.tmdb.org/t/p/original${movie.backdrop_path}`"
           class="h-full w-full object-cover object-top transition-transform duration-500 group-hover:scale-110"
         />
       </div>
+      <!-- Opacity Layer Div -->
       <div
         class="absolute inset-0 bg-black opacity-80 transition-opacity duration-1000 group-hover:opacity-30"
       ></div>
+      <!-- Title and Description Div -->
       <div
         class="absolute inset-0 flex gap-2 flex-col px-9 transition-all duration-500 justify-end mb-4"
       >
@@ -21,6 +24,42 @@
         >
           {{ movie.overview }}
         </p>
+        <!-- Tags Div -->
+        <div class="flex gap-2">
+          <div
+            v-for="genre in movie.genres"
+            :key="genre.id"
+            class="bg-black bg-opacity-40 px-4 py-1 rounded-lg text-sm"
+          >
+            <span class="opacity-100">{{ genre.name }}</span>
+          </div>
+        </div>
+      </div>
+      <!-- Details Div -->
+      <div
+        class="absolute h-full top-0 right-0 mx-4 flex flex-col justify-around z-10 font-bold"
+      >
+        <div
+          class="w-24 h-16 bg-yellow-400 bg-opacity-40 rounded-xl flex items-center justify-center"
+        >
+          {{ Math.floor(movie.vote_average * 10) }}%
+        </div>
+        <div
+          class="w-24 h-16 bg-black bg-opacity-40 rounded-xl flex items-center justify-center"
+        >
+          {{ movie.release_date.split('-')[0] }}
+        </div>
+        <div
+          class="w-24 h-16 bg-black bg-opacity-40 rounded-xl flex items-center justify-center"
+        >
+          {{ Math.floor(movie.runtime / 60) }}h {{ movie.runtime % 60 }}m
+        </div>
+        <div
+          class="w-24 h-16 bg-green-900 rounded-xl flex items-center justify-center"
+          @click="testEvent()"
+        >
+          <i class="fas fa-play text-3xl"></i>
+        </div>
       </div>
     </div>
   </div>
@@ -31,14 +70,27 @@ import axios from 'axios'
 
 const apiKey = import.meta.env.VITE_TMDB_API_KEY
 
+const getDetails = async (movie) => {
+  const result = await axios.get(
+    `https://api.themoviedb.org/3/movie/${movie.id}?api_key=${apiKey}&language=en-US`
+  )
+  return result.data
+}
+
 const getResults = async () => {
   const result = await axios.get(
     `https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`
   )
+
+  const movie = await getDetails(result.data.results[1])
   await new Promise((resolve) => setTimeout(resolve, 2000))
-  return result.data.results
+  return movie
 }
 
-const result = await getResults()
-const movie = result[0]
+const testEvent = () => {
+  alert('Hello World')
+}
+
+const movie = await getResults()
+console.log(movie)
 </script>
--- src/skeletons/FeaturedMovieSkeleton.vue
+++ src/skeletons/FeaturedMovieSkeleton.vue
@@ -10,6 +10,19 @@
         </h1>
         <UnitSkeleton class="rounded-lg max-w-[20%] text-lg" />
         <UnitSkeleton class="rounded-lg max-w-[50%] mb-4 text-lg" />
+        <div class="flex max-w-full gap-2">
+          <UnitSkeleton class="rounded-lg w-[5%] text-sm" />
+          <UnitSkeleton class="rounded-lg w-[5%] text-sm" />
+          <UnitSkeleton class="rounded-lg w-[5%] text-sm" />
+        </div>
+      </div>
+      <div
+        class="absolute h-full top-0 right-0 mx-4 flex flex-col justify-around z-10 font-bold"
+      >
+        <UnitSkeleton class="w-24 h-16 bg-black bg-opacity-40 rounded-xl" />
+        <UnitSkeleton class="w-24 h-16 bg-black bg-opacity-40 rounded-xl" />
+        <UnitSkeleton class="w-24 h-16 bg-black bg-opacity-40 rounded-xl" />
+        <UnitSkeleton class="w-24 h-16 bg-black bg-opacity-40 rounded-xl" />
       </div>
     </div>
   </div>
