21784a0bef8053b311dee49229b9f62af2527692
whitehead421 <huseyinakbas@posta.mu.edu.tr>
2023-06-02 03:29:41 +0300

    ♻️ chore: navigationguard added

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import HomeView from '../views/HomeView.vue'
 import MovieView from '../views/MovieView.vue'
 import ProfileView from '../views/ProfileView.vue'
@@ -37,6 +38,32 @@ const router = createRouter({
       component: LoginView,
     },
   ],
+})
+
+const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const removeListener = onAuthStateChanged(
+      getAuth(),
+      (user) => {
+        removeListener()
+        resolve(user)
+      },
+      reject
+    )
+  })
+}
+
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (await getCurrentUser()) {
+      next()
+    } else {
+      alert('You must be logged in to see this page.')
+      next('/')
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
