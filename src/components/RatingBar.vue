3afd9eaa3d6707c531f125c6fa2767b7cdfb8149
whitehead421 <huseyinakbas@posta.mu.edu.tr>
2023-06-04 04:24:12 +0300

    📦 refactor: add ratingbar component for profile

---
+++ src/components/RatingBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="my-2 grid grid-cols-3 items-center">
+    <span class="text-left text-sm">{{ genre.name }}</span>
+
+    <div class="h-2 flex-1 rounded-lg bg-gray-800">
+      <div :class="`h-full w-[10%] rounded-lg bg-secondary`"></div>
+    </div>
+    <div class="ml-2 flex flex-row items-center gap-1 text-sm text-gray-600">
+      <i class="fa-regular fa-star"></i>
+      <span class="flex flex-row gap-1">
+        <span>
+          {{ genre.rating }}
+        </span>
+        <span> ({{ genre.count }}) </span>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  genre: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -24,8 +24,8 @@
 
       <!-- User Related Content and Statistics -->
       <hr class="my-2 border-secondary opacity-30 sm:hidden" />
-      <div class="flex flex-col">
-        <h2 class="py-2 text-left font-bold">Top-Rated Genres</h2>
+      <div v-for="genre in genres" :key="genre.id" class="flex flex-col">
+        <RatingBarVue :genre="genre" />
       </div>
     </div>
   </div>
@@ -34,8 +34,19 @@
 <script setup>
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { ref, onMounted } from 'vue'
+import RatingBarVue from '../components/RatingBar.vue'
 
 const user = ref(null)
+
+const genres = [
+  { name: 'Action', rating: 7.5, count: 100, id: 1 },
+  { name: 'Comedy', rating: 8.2, count: 85, id: 2 },
+  { name: 'Drama', rating: 9.1, count: 120, id: 3 },
+  { name: 'Thriller', rating: 7.8, count: 95, id: 4 },
+  { name: 'Romance', rating: 8.5, count: 70, id: 5 },
+  { name: 'Science Fiction', rating: 8, count: 110, id: 6 },
+  { name: 'Horror', rating: 7.3, count: 75, id: 7 },
+]
 
 onMounted(() => {
   const auth = getAuth()
